4b3295f59c7875246ac8ed77568361289d543855
igormedeiroz <igormedeiroz@gmail.com>
2023-02-14 17:13:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents are cute');
-streamlit.hearder('My Parents are cute');
-streamlit.text('My Parents are cute');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
