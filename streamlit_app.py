7305a1e84e57393365309e3fa56b72f60ba47a31
igormedeiroz <igormedeiroz@gmail.com>
2023-02-15 15:42:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,6 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json())
 
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 #connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
